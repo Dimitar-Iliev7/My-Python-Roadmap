10f08f5225f2ae7246b1192b62802e37c94cc6bb
Dimitar Iliev <117073615+Dimitar-Iliev7@users.noreply.github.com>
2023-04-03 19:32:39 +0300

    Add files via upload

---
+++ Python Primitive Data Types/main.py
@@ -0,0 +1,36 @@
+# Text Type: str
+
+# Numeric Types: int, float, complex
+
+# Sequence Types:	list, tuple, range
+
+# Mapping Type:	dict
+
+# Set Types:	set, frozenset
+
+# Boolean Type:	bool
+
+# Binary Types:	bytes, bytearray, memoryview
+
+# None Type:	NoneType
+
+
+
+# Example:
+# Print the data type of the variable y and x;
+
+x = 5
+print(type(x))  # class 'int'
+
+y = "Hello"
+print(type(y)) # class 'str'
+
+
+# Example 2:
+# What is the data type of the mystery variable?
+mystery = 734_529.678 # FLOAT
+
+
+
+
+
